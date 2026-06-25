ff6b02c15601157fd95a40af5e6ee765ac0652d0
narendheran <naren15107@gmail.com>
2026-06-25 14:10:54 +0530

    commit

---
+++ src/DAY_6/Class_task/Month.java
@@ -0,0 +1,17 @@
+package DAY_6.Class_task;
+
+public class Month {
+    public static void main(String[] args){
+        for(int month=1;month<=12;month++){
+            for(int week=1;week<=4;week++){
+                for(int day=1;day<=7;day++){
+                    System.out.print("month: "+month +"week: "+week+ "day: "+day);
+                    System.out.println("come to college");
+                    System.out.println("eat and sleep ");
+                    System.out.println("go to home ");
+                }
+            }
+        }
+
+    }
+}
