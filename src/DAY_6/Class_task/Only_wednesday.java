aa138a516ebc839fbf302fbaa65b5a4711a9b4dd
narendheran <naren15107@gmail.com>
2026-06-27 09:19:47 +0530

    only wednesday created

---
+++ src/DAY_6/Class_task/Only_wednesday.java
@@ -0,0 +1,25 @@
+package DAY_6.Class_task;
+
+public class Only_wednesday {
+
+        public static void main(String[] args) {
+
+            for (int month = 1; month <= 12; month++) {
+
+                for (int week = 1; week <= 4; week++) {
+
+                    for (int day = 1; day <= 7; day++) {
+
+                        if (day == 4) {
+                            break; // Stop after Wednesday
+                        }
+
+                        System.out.println("Month " + month +
+                                " Week " + week +
+                                " Day " + day);
+                    }
+                }
+            }
+        }
+    }
+
