b7234e01fdeae35e98cbfb6776447c2b6181d0c6
narendheran <naren15107@gmail.com>
2026-06-27 09:14:19 +0530

    Skip februray created

---
+++ src/DAY_6/Class_task/Skip_february.java
@@ -0,0 +1,26 @@
+package DAY_6.Class_task;
+
+public class Skip_february {
+
+
+        public static void main(String[] args) {
+
+            for (int month = 1; month <= 12; month++) {
+
+                if (month == 2) {
+                    continue; // Skip February
+                }
+
+                for (int week = 1; week <= 4; week++) {
+
+                    for (int day = 1; day <= 7; day++) {
+
+                        System.out.println("Month " + month +
+                                " Week " + week +
+                                " Day " + day);
+                    }
+                }
+            }
+        }
+    }
+
