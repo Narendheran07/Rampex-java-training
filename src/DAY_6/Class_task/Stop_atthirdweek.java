450e594a9e4c1d1531021281712aaaf7ff57e9e6
narendheran <naren15107@gmail.com>
2026-06-27 09:18:15 +0530

    stop at third week created

---
+++ src/DAY_6/Class_task/Stop_atthirdweek.java
@@ -0,0 +1,26 @@
+package DAY_6.Class_task;
+
+public class Stop_atthirdweek {
+
+
+        public static void main(String[] args) {
+
+            for (int month = 1; month <= 12; month++) {
+
+                for (int week = 1; week <= 4; week++) {
+
+                    if (week == 3) {
+                        break; // Stop current month
+                    }
+
+                    for (int day = 1; day <= 7; day++) {
+
+                        System.out.println("Month " + month +
+                                " Week " + week +
+                                " Day " + day);
+                    }
+                }
+            }
+        }
+    }
+
