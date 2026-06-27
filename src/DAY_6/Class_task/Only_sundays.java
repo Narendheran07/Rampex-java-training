f2109c7d82124ff4b4b893518c8d9ac33d26b4f3
narendheran <naren15107@gmail.com>
2026-06-27 09:05:05 +0530

    only sunday  created

---
+++ src/DAY_6/Class_task/Only_sundays.java
@@ -0,0 +1,26 @@
+package DAY_6.Class_task;
+
+public class Only_sundays {
+
+
+        public static void main(String[] args) {
+
+            for (int month = 1; month <= 12; month++) {
+
+                for (int week = 1; week <= 4; week++) {
+
+                    for (int day = 1; day <= 7; day++) {
+
+                        if (day == 7) {
+                            System.out.println("Month " + month +
+                                    " Week " + week +
+                                    " Day " + day + " (Sunday)");
+                        }
+
+                    }
+                }
+            }
+
+        }
+    }
+
