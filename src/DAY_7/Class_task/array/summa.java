14110ae017276efc6ca421085c2be2eb2aaa15d4
narendheran <naren15107@gmail.com>
2026-06-29 09:14:15 +0530

    minimum created

---
+++ src/DAY_7/Class_task/array/summa.java
@@ -0,0 +1,16 @@
+package DAY_7.Class_task.array;
+import java.util.Scanner;
+
+public class summa {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+
+        System.out.print("Enter the name : ");
+        String name=sc.nextLine();
+        System.out.print("enter your age: ");
+        int age=sc.nextInt();
+        System.out.println("Name is "+name);
+        System.out.println("my Age is "+age);
+
+    }
+}
