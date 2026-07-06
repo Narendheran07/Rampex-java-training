02c072b137ee9a48e29323bef73e1ea81da0a231
narendheran <naren15107@gmail.com>
2026-07-06 11:15:58 +0530

    firsst program

--- src/DAY_1/Class_task/Me.java
+++ src/DAY_1/Class_task/Me.java
@@ -1,4 +1,10 @@
 package DAY_1.Class_task;
 
 public class Me {
-}
+
+        public static void main(String[] args)
+        {
+            System.out.println(" This is my first program ' Narendheran ' ");
+        }
+    }
+
