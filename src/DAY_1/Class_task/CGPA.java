0bf4257faf86d0e1038193a10106203bd09445fa
narendheran <naren15107@gmail.com>
2026-07-06 09:22:31 +0530

    Program to caluclate my CGPA

---
+++ src/DAY_1/Class_task/CGPA.java
@@ -0,0 +1,12 @@
+package DAY_1.Class_task;
+
+public class CGPA {
+
+        public static void main(String[] args)
+        {
+            int age =19;
+            double cgpa = 8.4;
+            System.out.println("My age is "+ age + " and cgpa is " + cgpa);
+        }
+    }
+
