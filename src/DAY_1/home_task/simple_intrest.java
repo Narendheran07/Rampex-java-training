1e0503f7be0fdea884f9ab3dd6d32fc7df4ad03c
narendheran <naren15107@gmail.com>
2026-07-06 11:18:49 +0530

    This program to show the Simple intrest

---
+++ src/DAY_1/home_task/simple_intrest.java
@@ -0,0 +1,13 @@
+package DAY_1.home_task;
+
+public class simple_intrest {
+
+        public static void main(String[] args) {
+            int p = 10000;
+            int n = 5;
+            double r = 5.0;
+            double simpleInterest = (p * n * r) / 100;
+            System.out.println("The SI is: " + simpleInterest);
+        }
+    }
+
