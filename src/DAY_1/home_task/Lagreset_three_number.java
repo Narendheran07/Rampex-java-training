ae494ebd55cb3fc61150a0bb34a6b2bcca486dc8
narendheran <naren15107@gmail.com>
2026-07-06 11:24:10 +0530

    program to find largest three number

---
+++ src/DAY_1/home_task/Lagreset_three_number.java
@@ -0,0 +1,20 @@
+package DAY_1.home_task;
+
+public class Lagreset_three_number {
+
+        public static void main(String[] args) {
+            int a = 10, b = 20, c = 30;
+            if (a > b && a > c) {
+                System.out.println("The largest is: " + a);
+            }
+            else if (b > a && b > c) {
+                System.out.println("The largest is: " + b);
+            }
+            else {
+                System.out.println("The largest is: " + c);
+            }
+        }
+
+    }
+
+
