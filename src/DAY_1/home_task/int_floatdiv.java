a4c41c13b8e241a753d376bb0d67633ecb4b1367
narendheran <naren15107@gmail.com>
2026-07-06 11:21:53 +0530

    rogram for INT & FLOAT division

---
+++ src/DAY_1/home_task/int_floatdiv.java
@@ -0,0 +1,19 @@
+package DAY_1.home_task;
+
+public class int_floatdiv {
+
+        public static void main(String[] args) {
+            int num1 = 5;
+            int num2 = 2;
+
+            double dnum1 = 5.0;
+            double dnum2 = 2.0;
+            int iresult = num1 / num2;
+            float fresult = (float) (dnum1 / dnum2);
+
+            System.out.println("Int Div (5/2) : " + iresult);
+            System.out.println("Float Div (5.0/2.0) : " + fresult);
+        }
+    }
+
+
