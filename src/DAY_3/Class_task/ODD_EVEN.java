8f75e1d66774d2bb3f32e5ac14dbe6ee70d57b19
narendheran <naren15107@gmail.com>
2026-07-06 13:53:31 +0530

    Basici Even and Odd number program

---
+++ src/DAY_3/Class_task/ODD_EVEN.java
@@ -0,0 +1,22 @@
+package DAY_3.Class_task;
+import java.util.Scanner;
+
+
+public class ODD_EVEN {
+
+
+        public static void main(String[] args){
+            Scanner sc= new Scanner(System.in);
+            System.out.println("Enter number : ");
+            int a= sc.nextInt();
+
+            if(a%2==0){
+                System.out.println("Even number");
+            }
+            else{
+                System.out.println("Odd number");
+            }
+            sc.close();
+        }
+    }
+
