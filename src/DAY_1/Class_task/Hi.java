32d0f3539a231897a71a8cf791db28dd9d5e0193
narendheran <naren15107@gmail.com>
2026-07-06 11:15:20 +0530

    This Program to Show HI

--- src/DAY_1/Class_task/Hi.java
+++ src/DAY_1/Class_task/Hi.java
@@ -1,4 +1,10 @@
 package DAY_1.Class_task;
 
 public class Hi {
-}
+
+        public static void main(String[] args)       // this is the function
+        {
+            System.out.println("HI");
+        }
+    }
+
