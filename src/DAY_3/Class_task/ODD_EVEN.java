f3112ed645f9a15807498f42a0118e94498282a1
narendheran <naren15107@gmail.com>
2026-07-06 14:29:58 +0530

    Positive Odd and Even

--- src/DAY_3/Class_task/ODD_EVEN.java
+++ src/DAY_3/Class_task/ODD_EVEN.java
@@ -18,5 +18,25 @@ public class ODD_EVEN {
             }
             sc.close();
         }
-    }
+
+    public static class If_else {
+
+            public static void main(String[] args){
+                Scanner scanner = new Scanner(System.in);
+                System.out.println("enter the number:");
+                int a=scanner.nextInt();
+
+                if(a>90){
+                    System.out.println("greater than 90");
+                }
+                else if(a>50){
+                    System.out.println("greater than 50");
+                }
+                else{
+                    System.out.println("greater than 10");
+                }
+                scanner.close();
+            }
+        }
+}
 
