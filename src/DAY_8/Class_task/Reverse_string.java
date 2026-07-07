bab0dd21b6b763c0f5398a1db05cdf1bd01bf9dc
narendheran <naren15107@gmail.com>
2026-07-07 14:01:01 +0530

    program for Reverse a String

---
+++ src/DAY_8/Class_task/Reverse_string.java
@@ -0,0 +1,17 @@
+package DAY_8.Class_task;
+
+public class Reverse_string {
+
+        public static void main(String[] args) {
+            String name = "anu";
+            String rev = "";
+            int len = name.length();
+
+            for(int i = len - 1; i >= 0; --i) {
+                rev = rev + name.charAt(i);
+            }
+
+            System.out.println(rev);
+        }
+    }
+
