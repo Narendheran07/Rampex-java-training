d7c03c2602e19001ae1778efdaf4716222a10a10
narendheran <naren15107@gmail.com>
2026-07-07 09:27:29 +0530

    Hello in loop

---
+++ src/DAY_4/Class_task/Hello_inloop.java
@@ -0,0 +1,10 @@
+package DAY_4.Class_task;
+
+public class Hello_inloop {
+            public static void main(String[] args){
+            for(int i=1;i<20;i++){
+                System.out.println("hello");
+            }
+        }
+    }
+
