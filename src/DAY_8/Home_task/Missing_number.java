feb2c29cc0d36edbd3fc9a4584f1f06d5b5433b3
narendheran <naren15107@gmail.com>
2026-07-07 14:26:46 +0530

    program to find the missing value

---
+++ src/DAY_8/Home_task/Missing_number.java
@@ -0,0 +1,22 @@
+package DAY_8.Home_task;
+
+public class Missing_number {
+
+        public static void main(String[] args){
+
+            int sum=0;
+            int asum=0;
+            int n=10;
+            int []arr={1,2,3,4,5,6,7,8,10};
+            for(int i=0;i<=n;i++){
+                asum+=i;
+            }
+            for(int i=0;i<arr.length;i++){
+                sum=sum+arr[i];
+            }
+            int missingnum=asum-sum;
+            System.out.println("the missing number from 1 to "+n+": " + missingnum);
+
+        }
+    }
+
