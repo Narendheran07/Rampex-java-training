3d3cfb566b0fafb7280b61c5f6a2dc75a6a1ce4f
narendheran <naren15107@gmail.com>
2026-07-07 10:04:10 +0530

    Example program for Continue

---
+++ src/DAY_6/Class_task/Continue.java
@@ -0,0 +1,14 @@
+package DAY_6.Class_task;
+
+public class Continue {
+
+        public static void main(String[] args){
+            for(int i=0;i<=5;i++){
+                if(i%2==0){
+                    continue;
+                }
+                System.out.println(i);
+            }
+        }
+    }
+
