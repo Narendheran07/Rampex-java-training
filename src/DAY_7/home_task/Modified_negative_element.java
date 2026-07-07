ca727c131d5b2ca80642f86db00e219cd83c0bed
narendheran <naren15107@gmail.com>
2026-07-07 11:24:00 +0530

    program to modify the negative number

---
+++ src/DAY_7/home_task/Modified_negative_element.java
@@ -0,0 +1,18 @@
+package DAY_7.home_task;
+import java.util.Arrays;
+
+public class Modified_negative_element {
+
+        public static void main(String[] args) {
+            int[] arr = new int[]{122, -23, 22, -98, -70};
+
+            for (int i = 0; i < arr.length; ++i) {
+                if (arr[i] < 0) {
+                    arr[i] = 0;
+                }
+            }
+
+            System.out.println(Arrays.toString(arr));
+        }
+    }
+
