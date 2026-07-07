7948cd3070aa5e2d300d98ef20903eb0f4f1e64c
narendheran <naren15107@gmail.com>
2026-07-07 10:31:59 +0530

    program to find maximum value

---
+++ src/DAY_7/Class_task/Max_finding.java
@@ -0,0 +1,20 @@
+package DAY_7.Class_task;
+
+public class Max_finding {
+
+        public static void main(String[] args) {
+            int[] arr = new int[]{1, 2, 3, 5, 4};
+            int max = 0;
+
+            for (int i = 0; i < arr.length; ++i) {
+                if (arr[i] > max) {
+                    max = arr[i];
+                }
+
+
+            }
+            System.out.println("max value is : " + max);
+
+        }
+    }
+
