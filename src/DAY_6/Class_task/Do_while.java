e7d1be3cd51b2c5a307b61028354f73dd4f5c505
narendheran <naren15107@gmail.com>
2026-07-07 10:06:33 +0530

    Example program for Do while

---
+++ src/DAY_6/Class_task/Do_while.java
@@ -0,0 +1,13 @@
+package DAY_6.Class_task;
+
+public class Do_while {
+
+        public static void main(String[] args) {
+            int i=1;
+            do{
+                System.out.println(i++);
+            }
+            while(i<=7);
+        }
+    }
+
