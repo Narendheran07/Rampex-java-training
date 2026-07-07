bda2ce81a31e64d7060f9a6adb7ae582c98618e6
narendheran <naren15107@gmail.com>
2026-07-07 10:02:44 +0530

    Example program for Break

---
+++ src/DAY_6/Class_task/Break.java
@@ -0,0 +1,14 @@
+package DAY_6.Class_task;
+
+public class Break {
+
+        public static void main(String[] args){
+            for(int i=1;i<=5;i++){
+                if(i==3){
+                    break;
+                }
+                System.out.println(i);
+            }
+        }
+    }
+
