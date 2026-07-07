973cb44c38a734ea484b6fdedc197d6300f0cffe
narendheran <naren15107@gmail.com>
2026-07-07 09:55:55 +0530

    program for odd Sum

---
+++ src/DAY_5/Class_task/Odd_Sum.java
@@ -0,0 +1,18 @@
+package DAY_5.Class_task;
+import java.util.Scanner;
+
+public class Odd_Sum {
+           public static void main(String[] args){
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter the number :");
+            int n=sc.nextInt();
+            int sum=0;
+            for(int i=1;i<=n;i++){
+                if(i%2!=0){
+                    sum+=i;
+                }
+            }
+            System.out.println("total sum :"+sum);
+            sc.close();
+        }
+    }
