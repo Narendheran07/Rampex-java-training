306b6b8f4814bbde05e860db803aa29f2e5fc5e4
narendheran <naren15107@gmail.com>
2026-07-07 09:47:30 +0530

    Even product program

---
+++ src/DAY_5/Class_task/Even_product.java
@@ -0,0 +1,22 @@
+package DAY_5.Class_task;
+
+
+import java.util.Scanner;
+public class Even_product {
+
+
+
+        public static void main(String[] args){
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter the number :");
+            int n=sc.nextInt();
+            int product=1;
+            for(int i=1;i<=n;i++){
+                if(i%2==0){
+                    product*=i;
+                }
+            }
+            System.out.println(product);
+            sc.close();
+        }
+    }
