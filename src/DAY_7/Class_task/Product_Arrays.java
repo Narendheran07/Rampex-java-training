33c44016fd700daf284ccbb1be451c95c61b8311
narendheran <naren15107@gmail.com>
2026-07-07 10:37:26 +0530

    program to print product of arrays

---
+++ src/DAY_7/Class_task/Product_Arrays.java
@@ -0,0 +1,20 @@
+package DAY_7.Class_task;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Product_Arrays {
+
+        public static void main(String[] args) {
+            new Scanner(System.in);
+            int product = 1;
+            int[] arr = new int[]{2,3,4,5,6};
+
+            for(int i = 0; i <= 4; ++i) {
+                product *= arr[i];
+            }
+
+            System.out.println(Arrays.toString(arr));
+            System.out.println(product);
+        }
+    }
+
