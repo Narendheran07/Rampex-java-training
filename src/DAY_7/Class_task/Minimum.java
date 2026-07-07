6d902162c5fadc88cc9e7128597b550b947bbb8c
narendheran <naren15107@gmail.com>
2026-07-07 10:34:11 +0530

    program to find minmum value

---
+++ src/DAY_7/Class_task/Minimum.java
@@ -0,0 +1,18 @@
+package DAY_7.Class_task;
+
+public class Minimum {
+
+        public static void main(String[] args) {
+            int[] arr = new int[]{1,2,3,5,4};
+            int min = arr[0];
+
+            for (int i = 0; i < arr.length; ++i) {
+                if (arr[i] < min) {
+                    min = arr[i];
+                }
+            }
+
+            System.out.println("The minimum value in the array is : " + min);
+        }
+    }
+
