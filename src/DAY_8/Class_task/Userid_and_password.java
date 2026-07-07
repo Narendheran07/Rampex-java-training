36326b6a74c2889ee8dcde771629cbcfd42d255c
narendheran <naren15107@gmail.com>
2026-07-07 14:05:27 +0530

    program for user id and password

---
+++ src/DAY_8/Class_task/Userid_and_password.java
@@ -0,0 +1,19 @@
+package DAY_8.Class_task;
+import java.util.Scanner;
+
+public class Userid_and_password {
+
+        public static void main(String[] args) {
+            String userId = "Sandy@123";
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter your user id : ");
+            String InputId = sc.nextLine();
+            if (InputId.equals(userId)) {
+                System.out.println("Welcome");
+            } else {
+                System.out.println("Enter correct user id");
+            }
+
+        }
+    }
+
