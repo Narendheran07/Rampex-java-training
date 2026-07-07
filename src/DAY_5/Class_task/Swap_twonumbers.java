99e0afb25cf94bdb79e7579947f9cdfb09f243ca
narendheran <naren15107@gmail.com>
2026-07-07 09:58:56 +0530

    Swapping of two number

---
+++ src/DAY_5/Class_task/Swap_twonumbers.java
@@ -0,0 +1,16 @@
+package DAY_5.Class_task;
+
+public class Swap_twonumbers {
+
+        public static void main(String[] args) {
+            int a = 5;
+            int b = 10;
+            int temp;
+            temp = a;
+            a = b;
+            b = temp;
+            System.out.println(a);
+            System.out.println(b);
+        }
+    }
+
