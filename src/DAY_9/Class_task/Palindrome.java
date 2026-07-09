566951781f890144a9d9c15ba91bf35662d75ecd
narendheran <naren15107@gmail.com>
2026-07-09 14:24:09 +0530

    program to print palindrome

---
+++ src/DAY_9/Class_task/Palindrome.java
@@ -0,0 +1,25 @@
+package DAY_9.Class_task;
+import java.util.Scanner;
+
+public class Palindrome {
+
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter the string : ");
+            String org = sc.nextLine();
+            String rev = "";
+
+            for (int i = org.length() - 1; i >= 0; --i) {
+                rev = rev + org.charAt(i);
+            }
+
+            System.out.println(rev);
+            if (rev.equalsIgnoreCase(org)) {
+                System.out.println("It is palindrome");
+            } else {
+                System.out.println("it is not palindrome");
+            }
+
+        }
+    }
+
--- src/DAY_9/Class_task/long_String.java
+++
@@ -1,4 +0,0 @@
-package DAY_9.Class_task;
-
-public class long_String {
-}
