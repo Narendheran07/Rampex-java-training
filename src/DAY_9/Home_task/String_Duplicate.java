cf9588c66ada5e2ec2a9874a0ae31da2bd99be13
narendheran <naren15107@gmail.com>
2026-07-09 14:29:21 +0530

    program to Find the duplicate String

---
+++ src/DAY_9/Home_task/String_Duplicate.java
@@ -0,0 +1,20 @@
+package DAY_9.Home_task;
+
+public class String_Duplicate {
+
+        public static void main(String[] args) {
+            String[] arr = new String[]{"sam", "jane", "sam", "jane", "sam"};
+            String n = "";
+
+            for (int i = 0; i < arr.length; ++i) {
+                for (int j = 0; j < i; ++j) {
+                    if (arr[i] == arr[j] && i != j) {
+                        System.out.println(arr[j]);
+                        break;
+                    }
+                }
+            }
+
+        }
+    }
+
