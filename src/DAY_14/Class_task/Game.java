f4ce3890e759131187b4d29f5127404489d8e484
narendheran <naren15107@gmail.com>
2026-07-11 11:47:56 +0530

    Program for game with random numbers

---
+++ src/DAY_14/Class_task/Game.java
@@ -0,0 +1,20 @@
+package DAY_14.Class_task;
+import java.util.*;
+
+public class Game {
+
+        public static void main(String[] args){
+            int rnum = (int)(10*Math.random());
+            Scanner sc = new Scanner(System.in);
+            System.out.println("guess the rnum????");
+            int num = sc.nextInt();
+            if(num == rnum){
+                System.out.println("You won!!");
+            }else{
+                System.out.println("You lost!!" + rnum);
+            }
+
+
+        }
+
+}
