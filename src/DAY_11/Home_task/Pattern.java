b6743e1f2e935aabd01ac1c84b399d74d23203af
narendheran <naren15107@gmail.com>
2026-07-11 09:45:59 +0530

    Program for Printing the pattern

---
+++ src/DAY_11/Home_task/Pattern.java
@@ -0,0 +1,14 @@
+package DAY_11.Home_task;
+
+public class Pattern {
+
+        public static void main(String[] args){
+            for(int i=1;i<=5;i++){
+                for(int j=1;j<=6;j++){
+                    System.out.println("R" + i + "S" + j );
+                }
+                System.out.println();
+            }
+        }
+
+}
