30ec6756ac8f7ae407c922e2d03cb9c75dc42ffb
narendheran <naren15107@gmail.com>
2026-07-11 09:52:27 +0530

    Program for Parentreference

---
+++ src/DAY_12/Home_task/ParentReference.java
@@ -0,0 +1,29 @@
+package DAY_12.Home_task;
+
+public class ParentReference {
+
+
+        public static void main(String[] args) {
+
+            Animal c1 = new Cat();
+            Animal c2= new Cat();
+
+            c1.start();
+            c2.start();
+
+        }
+    }
+    class Animal {
+
+        void start() {
+            System.out.println("Animal");
+        }
+    }
+
+    class Cat extends Animal {
+
+        void start() {
+            System.out.println("cat");
+        }
+
+}
