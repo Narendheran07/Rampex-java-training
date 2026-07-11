a2ed94e2438f552ed2588a1ca863b104b73c7d91
narendheran <naren15107@gmail.com>
2026-07-11 09:51:34 +0530

    Program for Overriding

---
+++ src/DAY_12/Home_task/overriding.java
@@ -0,0 +1,25 @@
+package DAY_12.Home_task;
+
+public class overriding {
+
+        public static void main(String[] args) {
+
+            vehicle v1 = new car();
+
+            v1.start();
+        }
+    }
+    class vehicle {
+
+        void start() {
+            System.out.println("vehicle Started");
+        }
+    }
+
+    class car extends vehicle {
+
+        void start() {
+            System.out.println(" Car Started");
+        }
+
+}
