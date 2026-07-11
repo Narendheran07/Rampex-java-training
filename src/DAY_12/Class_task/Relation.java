075b78bd331461af3d5c7956141037b0d48ef662
narendheran <naren15107@gmail.com>
2026-07-11 09:49:10 +0530

    Program for Is A relation

---
+++ src/DAY_12/Class_task/Relation.java
@@ -0,0 +1,24 @@
+package DAY_12.Class_task;
+
+public class Relation {
+
+        public static void main(String[] args){
+            Car c1 = new Car();
+            c1.Start();
+
+
+        }
+    }
+    class Car{
+        String brand;
+        String model;
+        public static void Start(){
+            System.out.println("Car Stared");
+        }
+    }
+    class TaTaCar extends Car{
+        public static void Start(){
+            System.out.println("Car Starting");
+        }
+
+}
