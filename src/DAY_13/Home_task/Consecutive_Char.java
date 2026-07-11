fad775a6d2daa022492d6c4cf039f834d683b362
narendheran <naren15107@gmail.com>
2026-07-11 10:28:16 +0530

    Program for Consecutive character

---
+++ src/DAY_13/Home_task/Consecutive_Char.java
@@ -0,0 +1,20 @@
+package DAY_13.Home_task;
+
+public class Consecutive_Char {
+
+        public static void main(String[] args){
+            String s = "aaabbccccdd";
+            char[] arr = s.toCharArray();
+            int count = 1;
+            for(int i=0;i<arr.length;i++){
+                if(i<arr.length - 1 && arr[i] == arr[i+1]) {
+                    count++;
+                }else{
+                    System.out.print(arr[i] + "" + count );
+                    count = 1;
+                }
+            }
+
+        }
+
+}
