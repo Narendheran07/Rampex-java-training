461a89bc8f48a7a664b703d161b84765bc7e09a0
narendheran <naren15107@gmail.com>
2026-07-11 12:06:54 +0530

    program for add and remove in ArrayLit

---
+++ src/DAY_16/Home_task/TrainCocahManagement.java
@@ -0,0 +1,17 @@
+package DAY_16.Home_task;
+import java.util.ArrayList;
+
+public class TrainCocahManagement {
+
+        public static void main(String[] args){
+            ArrayList<String> n = new ArrayList<>();
+            n.add(0,"Engine");
+            n.add(1,"Coach 2");
+            n.add(2,"Coach 1");
+            n.add(3,"Guard");
+            n.remove(3);
+            System.out.println(n);
+
+        }
+    }
+
