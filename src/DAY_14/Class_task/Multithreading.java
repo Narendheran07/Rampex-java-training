e5b9c72f88e0191cd46927043f20c04eba20be7d
narendheran <naren15107@gmail.com>
2026-07-11 11:48:54 +0530

    Program for mulitiThreading

---
+++ src/DAY_14/Class_task/Multithreading.java
@@ -0,0 +1,10 @@
+package DAY_14.Class_task;
+
+public class Multithreading {
+
+        public static void main(String[] args){
+            Thread t = Thread.currentThread();
+            System.out.println(t.getName());
+        }
+
+}
