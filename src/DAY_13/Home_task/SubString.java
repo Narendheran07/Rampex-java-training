70c26af98adfcaeb6f9617e26767ca9b98b22c29
narendheran <naren15107@gmail.com>
2026-07-11 10:34:00 +0530

    Program for printing SubString

---
+++ src/DAY_13/Home_task/SubString.java
@@ -0,0 +1,15 @@
+package DAY_13.Home_task;
+
+public class SubString {
+
+        public static void main(String[] args){
+            String a = "abc";
+            for(int i=0;i<a.length();i++){
+                for(int j=i+1;j<=a.length();j++){
+                    System.out.println(a.substring(i,j));
+                }
+            }
+
+        }
+    }
+
