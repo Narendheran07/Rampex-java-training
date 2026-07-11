8469d3e73a0ddb7b5a0a538c5e0e43d90307ca26
narendheran <naren15107@gmail.com>
2026-07-11 10:33:11 +0530

    Program for Reverse the String

---
+++ src/DAY_13/Home_task/Reverse_string.java
@@ -0,0 +1,24 @@
+package DAY_13.Home_task;
+import java.util.Arrays;
+
+public class Reverse_string {
+
+        public static void main(String[] args){
+            RevereWord();
+
+        }
+        public static void RevereWord(){
+            String str = "Welcome to chennai";
+            String[]  word = str.split(" ");
+            for(String words : word){
+                for(int i=words.length() -1;i>=0;i--) {
+                    System.out.print(words.charAt(i));
+
+                }
+                System.out.print(" ");
+
+            }
+        }
+
+
+}
