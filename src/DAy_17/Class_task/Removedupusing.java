130a6f9c21ae38de020b54fa157d936e94e3109a
narendheran <naren15107@gmail.com>
2026-07-13 09:47:14 +0530

    program for Sample

---
+++ src/DAy_17/Class_task/Removedupusing.java
@@ -0,0 +1,24 @@
+package DAy_17.Class_task;
+import java.util.Arrays;
+import java.util.TreeSet;
+
+public class Removedupusing {
+
+        public static void main(String[] args){
+            int[] arr = {1, 2, 3, 4, 4};
+            removedup(arr);
+            System.out.print(Arrays.toString(arr));
+        }
+        public static void  removedup(int[] nums){
+            TreeSet<Integer> ts = new TreeSet<>();
+            for(Integer i:nums) {
+                ts.add(i);
+            }
+            int index = 0;
+            for(Integer i:ts) {
+                nums[index] = i;
+                index++;
+            }
+        }
+
+}
