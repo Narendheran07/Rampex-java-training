502f910c7aaa9d4faced848d03d36558db146525
narendheran <naren15107@gmail.com>
2026-07-14 18:03:33 +0530

    program for group word based on length

---
+++ src/DAY_19/Home_task/Groupbasedonlength.java
@@ -0,0 +1,24 @@
+package DAY_19.Home_task;
+import java.util.ArrayList;
+
+public class Groupbasedonlength {
+
+        public static void main(String[] args){
+            String[] word = {"Java", "API", "Spring", "Collection", "Map", "Queue"};
+            ArrayList<String> str = new ArrayList<>();
+            for(String i:word){
+                str.add(i);
+            }
+            for(String i:word){
+                int n = i.length();
+                for(String j:word){
+                    if(j.length() == n) {
+                        System.out.println(i);
+                    }
+
+                }
+                //System.out.println(i + "->" + i.length());
+            }
+        }
+
+}
