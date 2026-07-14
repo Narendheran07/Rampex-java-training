3360d5d6f37991310192570c499177c5bcc1043f
narendheran <naren15107@gmail.com>
2026-07-14 17:46:19 +0530

    program to print frequency in String using HashMap

---
+++ src/DAY_18/Class_task/FrequencyHashmap.java
@@ -0,0 +1,20 @@
+package DAY_18.Class_task;
+import java.util.HashMap;
+
+public class FrequencyHashmap {
+
+        public static void main(String[] args){
+            HashMap<Character,Integer> a = new HashMap<>();
+            String name = "saravanan";
+            char[] arr = name.toCharArray();
+            for(Character i:arr){
+                if(a.containsKey(i)){
+                    a.put(i,(Integer)(a.get(i))+1);
+                }else{
+                    a.put(i,1);
+                }
+            }
+            System.out.println(a);
+        }
+    }
+
