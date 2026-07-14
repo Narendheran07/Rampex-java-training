bd4a95b0be16f79d8a8355b0ac9d70d6857705c8
narendheran <naren15107@gmail.com>
2026-07-14 17:48:58 +0530

    program for find max mark

---
+++ src/DAY_18/Class_task/Hashmapmark.java
@@ -0,0 +1,27 @@
+package DAY_18.Class_task;
+import java.util.Map;
+import java.util.HashMap;
+
+public class Hashmapmark {
+
+        public static void main(String[] args){
+            HashMap<String,Integer> mark = new HashMap<>();
+            mark.put("priya",90);
+            mark.put("pavitha",70);
+            mark.put("ramya",50);
+            mark.put("moni",85);
+
+            int max = 0;
+            String name="";
+            for(Map.Entry<String,Integer> e:mark.entrySet()){
+                if(e.getValue() > max){
+                    max = e.getValue();
+                    name = e.getKey();
+                }
+            }
+
+            System.out.println(name);
+            System.out.println(max);
+        }
+
+}
