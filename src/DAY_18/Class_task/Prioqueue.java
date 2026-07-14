5bc527215a829ff7dca5899d1237767a3ca113d0
narendheran <naren15107@gmail.com>
2026-07-14 17:52:48 +0530

    program for priority queue

---
+++ src/DAY_18/Class_task/Prioqueue.java
@@ -0,0 +1,22 @@
+package DAY_18.Class_task;
+import java.util.*;
+import java.util.PriorityQueue;
+
+public class Prioqueue {
+
+        public static void main(String[] args){
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter the size:");
+            int size = sc.nextInt();
+            System.out.println("Enter the Element:");
+            //int[] n = new int[size];
+            PriorityQueue<Integer> a = new PriorityQueue<>();
+            for(int i=0;i<size;i++){
+                a.add(sc.nextInt());
+            }
+            System.out.println(a);
+
+
+        }
+
+}
